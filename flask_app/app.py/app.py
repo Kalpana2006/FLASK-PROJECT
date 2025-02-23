a9c7eada4bfbab97786d41db3e041e431b34ac41
Kalpana2006 <kalpanagohil246@gmail.com>
2025-02-23 20:53:38 +0530

    Update app.py

---
+++ flask_app/app.py/app.py
@@ -0,0 +1,14 @@
+from webapp import create_app
+from dotenv import find_dotenv, load_dotenv
+import os 
+
+app = create_app()
+load_dotenv(find_dotenv())
+
+debug = False
+
+if os.environ.get("ENVIRONMENT") == "development":
+    debug = True
+
+if __name__ == "__main__":
+    app.run(debug=debug)
